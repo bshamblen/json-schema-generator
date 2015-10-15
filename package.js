86ea6280f243d60b13a2258555a82c152b8c9654
Brian Shamblen <brian@sadabri.com>
2015-10-15 10:13:23 -0700

    Added Atmosphere package.js file

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+	name: 'bshamblen:json-schema-generator',
+	version: '0.0.1',
+	summary: 'Converts a JSON document to a JSON Schema.',
+	git: 'https://github.com/Yodata/json-schema-generator.git',
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+	api.export(['JSONSchemaGenerator'], ['client', 'server']);
+	api.addFiles(['./src/yodata.client.js']);
+});
